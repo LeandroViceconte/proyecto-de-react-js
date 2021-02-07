2b7dfac471c82a1101399439080d5ddbe7760fc5
Leandro Viceconte <leandrov.1791@gmail.com>
2021-02-07 15:14:20 -0300

    tercer commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,18 @@
 import styles from './NavBar.module.scss';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
 const NavBar = ({navbar, logo, options}) => {
     return (
         <nav className={styles.navbar}>
-            <div className={logo}>Logo </div>
-            <ul className={options}>
+            <div className={styles.logo}> logo </div>
+            <ul className={styles.options}>
                 <li><a href="">Poductos</a></li>
                 <li><a href="">Consescionarios</a></li>
                 <li><a href="">contactanos</a></li>
+                <li><ShoppingCartIcon /></li>
             </ul>
         </nav>
-
+        
     );
 }
 
